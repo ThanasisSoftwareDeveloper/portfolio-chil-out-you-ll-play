26f91abe2e5fa76d9db1f4f688904c12ec9aa494
Thanasis <80177359+ThanasisSoftwareDeveloper@users.noreply.github.com>
2025-11-08 21:22:08 +0200

    footer.php

--- site_portfolio/includes/footer.php
+++ site_portfolio/includes/footer.php
@@ -1,12 +1,14 @@
 <?php
 // includes/footer.php
 include_once 'counter.php';
+$footerCount = isset($visitor_count) ? (int) $visitor_count : get_site_counter();
 ?>
   </main>
 
   <footer class="site-footer">
-    <span>Page views: <?= $visitor_count ?? '0' ?></span>
-    <span>© 2025 Created by Thanasis Koufos</span>
+    <span class="footer-counter">Page views: <?= htmlspecialchars((string) $footerCount) ?></span>
+    <span class="footer-copy">© 2025 Created by Thanasis Koufos</span>
   </footer>
 </body>
 </html>
+
